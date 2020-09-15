3c4984a755acf40acda8dcc24c737fe42b2322af
shailza85 <33739384+shailza85@users.noreply.github.com>
2020-09-15 12:31:37 -0600

    Initial commit.

---
+++ LambdaSyntax/LambdaSyntax/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LambdaSyntax
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Lambda expression syntax being used with LINQ aggregates (Sum, Count, Min, Max) on a Collection.
+
+           
+
+       
+        }
+    }
+}
